98dc69a33d58983d2817464d5aae4a4573c785a8
Unknown <jvictor2985@gmail.com>
2019-10-03 16:14:30 -0300

    Versão final

--- ionic/mainhas/src/app/tab1/tab1.page.ts
+++ ionic/mainhas/src/app/tab1/tab1.page.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import { Map, latLng, tileLayer, Layer, marker, GeoSearchControl, OpenStreetMapProvider } from 'leaflet';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
   map: Map;
 
-  ionViewDidEnter() { this.leafletMap(); }
+  creches = [{ "id": "creche-menino-jesus", "name": "Creche Menino Jesus", "lat": -8.0389406, "lon": -34.9175401 },
+  { "id": "creche-paulo-guerra", "name": "Creche Senador Paulo Guerra", "lat": -8.0873369, "lon": -34.9069596 },
+  { "id": "creche-irma-dulce", "name": "Creche Irmã Dulce", "lat": -8.0421904, "lon": -34.8900877 }];
 
+  private userLatitude: any;
+  private userLongitude: any;
+
+  ionViewDidEnter() { this.leafletMap(); }
   leafletMap() {
     // In setView add latLng and zoom
-    this.map = new Map('mapId').setView([ -8.05428, -34.8813], 10);
-    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    this.map = new Map('mapId').setView([-8.05389, -34.88111], 13);
+    tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(this.map);
 
 
-    marker([28.6, 77]).addTo(this.map)
-      .bindPopup('Ionic 4 <br> Leaflet.')
-      .openPopup();
+
+    /*marker([-8.0389406, -34.9175401]).addTo(this.map)
+      .bindPopup('Creche Menino Jesus');
+    marker([-8.0873369, -34.9069596]).addTo(this.map)
+      .bindPopup('Creche Senador Paulo Guerra');
+    marker([-8.0421904, -34.8900877]).addTo(this.map)
+      .bindPopup('Creche Irmã Dulce');
+*/
+    for (let index = 0; index < this.creches.length; index++) {
+      marker([this.creches[index].lat, this.creches[index].lon])
+        .bindPopup(this.creches[index].name)
+        .addTo(this.map);
+    }
   }
 
   /** Remove map when we have multiple map object */
--- ionic/mainhas/src/app/tab2/tab2.page.ts
+++ ionic/mainhas/src/app/tab2/tab2.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor() {}
-
+ 
+  posts = [{"nome":"Gabi","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto1.jpeg"},
+  {"nome":"Madu","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto2.jpeg"},
+  {"nome":"Jessica","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto3.jpeg"}]
+   constructor() {}
+  
+   Postar(){
+    var descricao = document.getElementById('descricaoPost').value;
+    console.log(descricao);
+    console.log(this.posts);
+    this.posts = [{"nome":"Gabi","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto1.jpeg"},
+    {"nome":"Madu","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto2.jpeg"},
+    {"nome":"Jessica","descricao":"ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo,ela é mãe solo", "foto":"../../assets/images/foto3.jpeg"},
+    {"nome":"Gabi","descricao":descricao, "foto":"../../assets/images/foto1.jpeg"}];
+     document.getElementById('descricaoPost').innerText = '';
+   }
+ 
 }
